545e3cd55ca960880292a65d59271630e50cae90
Kelly Egan <kelly.egan@gmail.com>
2016-04-14 10:38:29 -0400

    Change name of callback to match adafruit io callback

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
     var URL = "1I6tyum4ae2d2mlNhdH5Q6nARLSyNNFtve09_GO5yS0I"
-    Tabletop.init( { key: URL, callback: loadSheet, simpleSheet: true } )
+    Tabletop.init( { key: URL, callback: success, simpleSheet: true } )
 })
 
 
-function loadSheet(data) {
+function success(data) {
   for(var i = 0; i < data.length; i++) {
     console.log(data[i]);
   }
