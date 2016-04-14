51af32bdaafbd3f4d4fdf41f544ee787a71a7ab3
Kelly Egan <kelly.egan@gmail.com>
2016-04-14 09:15:42 -0400

    Add call back for data loaded from Google spreadsheet

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
     var URL = ""
-    Tabletop.init( { key: URL, callback: loadBooks, simpleSheet: true } )
+    Tabletop.init( { key: URL, callback: loadSheet, simpleSheet: true } )
 })
+
+
+function loadSheet(data) {
+  for(var i = 0; i < data.length; i++) {
+    console.log(data[i]);
+  }
+}
